2fa44f28a1dce120201010f352965aa237559048
whatdoyouseedoc <whatdoyouseedoc@gmail.com>
2020-04-05 23:42:38 +0300

    add heroku procfile

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use('/assets', express.static(`${__dirname}/public`));
 
@@ -9,4 +9,4 @@ app.get('/', (req, res) => {
     res.redirect('/assets/index.html');
 });
 
-app.listen(port);
+app.listen(PORT);
