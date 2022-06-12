863f2a76ec37833523e396c6f442734d2a2c004e
whatdoyouseedoc <whatdoyouseedoc@gmail.com>
2022-06-13 00:49:43 +0400

    switch module to commonjs

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
-import express from 'express';
+const express = require('express');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+console.log(__dirname);
 
 app.use('/assets', express.static(`${__dirname}/public`));
 
